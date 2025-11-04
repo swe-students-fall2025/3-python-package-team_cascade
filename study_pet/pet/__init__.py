51112ea0caa31cc0a180a957c9ee02b8021652fa
Zeba Shafi <zs1329@nyu.edu>
2025-11-04 16:37:50 -0500

    Revert "Feat: Add get_encouragement function to pet"

--- study_pet/pet/__init__.py
+++ study_pet/pet/__init__.py
@@ -1,12 +1,10 @@
-from .core import get_status, check_daily_mood_decay, update_pet
-from .actions import rename_pet, feed_pet, set_morph, get_encouragement
+from .core import get_status, check_daily_mood_decay
+from .actions import rename_pet, feed_pet, set_morph
 
 __all__ = [
     "get_status",
     "rename_pet",
     "feed_pet",
     "set_morph",
-    "get_encouragement",
     "check_daily_mood_decay",
-    "update_pet"
 ]
